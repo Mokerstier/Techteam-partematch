230c42f10cd1092799d523a9d0f70079612e8b1a
Mokerstier <pannekoek-wouter@hotmail.com>
2019-05-09 14:53:11 +0200

    server is now running with 404

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, () => console.log(`server is gestart op port ${port}`));
+
 const camelCase = require('camelcase');
 
-console.log(camelCase("CMD-Proffesional"))
+app
+    .set("view engine", "ejs")
+
+    .get("/", home)
+    .get("/register", fetchRegister)
+    
+    .use(function (req, res, next ){
+        res.status(404).send("Sorry the page your looking for does not exist?!")
+    });
+;
+function home(req, res) {
+    res.render('index.ejs')
+  }
+function fetchRegister(req, res) {
+    res.render('register.ejs')
+}
