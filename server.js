d1bf5b86ba083e3e7e45fff86fc27e3532726daf
Mokerstier <pannekoek-wouter@hotmail.com>
2019-05-09 15:55:16 +0200

    added styles and made it work with .ejs file, also changed index.js -> server.js and changed routes on scripts in package.json

--- server.js
+++ server.js
@@ -7,13 +7,15 @@ app.listen(port, () => console.log(`server is gestart op port ${port}`));
 const camelCase = require('camelcase');
 
 app
+    
     .set("view engine", "ejs")
 
     .get("/", home)
     .get("/register", fetchRegister)
     
+    .use('/public', express.static('public'))
     .use(function (req, res, next ){
-        res.status(404).send("Sorry the page your looking for does not exist?!")
+        res.status(404).send("Sorry the page you're looking for does not exist?!")
     });
 ;
 function home(req, res) {
