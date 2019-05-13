dc3e9b72bdcbff6915ba7c11ebd05b76697bdf24
Mokerstier <pannekoek-wouter@hotmail.com>
2019-05-13 19:45:57 +0200

    added partials and renamed server.js

--- server.js
+++ server.js
@@ -7,13 +7,16 @@ app.listen(port, () => console.log(`server is gestart op port ${port}`));
 const camelCase = require('camelcase');
 
 app
-    
+    // define template engine
     .set("view engine", "ejs")
-
+    
+    // pages
     .get("/", home)
     .get("/register", fetchRegister)
     
     .use('/public', express.static('public'))
+    
+    // 404 redirect
     .use(function (req, res, next ){
         res.status(404).send("Sorry the page you're looking for does not exist?!")
     });
