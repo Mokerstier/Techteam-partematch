16a759c632a765e792691e3378acdb91907afe21
Mokerstier <pannekoek-wouter@hotmail.com>
2019-05-16 20:41:18 +0200

    added allot of content and made it possible to get data from form with post method

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const app = express();
-const port = 3000;
-
-app.listen(port, () => console.log(`server is gestart op port ${port}`));
-
 const camelCase = require('camelcase');
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+const port = 3000;
+const app = express();
 
 app
+    .use(express.static(__dirname, +'/public'))    
     
     // define template engine
     .set("view engine", "ejs")
@@ -14,20 +15,25 @@ app
     // pages
     .get("/", home)
     .get("/register", fetchRegister)
-    
-    .use(express.static(__dirname, +'/public'))
-    //.use('/public', express.static('public'))
-    
-    // 404 redirect
+    .post("/register", urlencodedParser, register)
+
     .use(function (req, res, next ){
         res.status(404).send("Sorry the page you're looking for does not exist?!")
     });
 ;
 function home(req, res) {
     res.render('pages/index.ejs' ,{
-        title: "Home"
+        title: "Home",
     });
   }
 function fetchRegister(req, res) {
-    res.render('pages/register.ejs')
+    res.render('pages/register.ejs' ,{
+        title: "Register"
+    });
 }
+function register(req, res){
+    console.log(req.body)
+    res.render('pages/register-succes.ejs')
+}
+
+app.listen(port, () => console.log(`server is gestart op port ${port}`));
