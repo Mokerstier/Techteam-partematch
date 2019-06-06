424a1b1ce948069717ddc3f3cdf8dcc2fb763b49
Mokerstier <pannekoek-wouter@hotmail.com>
2019-06-06 14:46:37 +0200

    found some options var that helps getting rid of db-errors

--- server.js
+++ server.js
@@ -17,11 +17,23 @@ const user = require('./controllers/users');
 require('dotenv').config();
 require('./controllers/user-login')(passport);
 
+const options = {
+  useNewUrlParser: true,
+  autoIndex: false, // Don't build indexes
+  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+  reconnectInterval: 500, // Reconnect every 500ms
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0,
+  connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
+  socketTimeoutMS: 45000, 
+  family: 4 // Use IPv4, skip trying IPv6
+};
 
 // Settings for online DATABASE
 var uri = process.env.MONGODB_URI;
 mongoose.set("useNewUrlParser", true);
-mongoose.connect(uri);
+mongoose.connect(uri, options);
 
 
 // LOCAL DATABASE
