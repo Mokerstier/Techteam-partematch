20423461d4d9ab2adad3e08ddfaddc7ff296cba0
Wouter <33430655+Mokerstier@users.noreply.github.com>
2019-06-13 14:47:05 +0200

    :bug: Fixing duplicate rules on server
    uri to db was declared multiple times

--- server.js
+++ server.js
@@ -30,35 +30,12 @@ const options = {
   family: 4 // Use IPv4, skip trying IPv6
 };
 
-<<<<<<< HEAD
-// // Settings for online DATABASE
-var uri = process.env.MONGODB_URI;
-// mongoose.set("useNewUrlParser", true);
-mongoose.connect(uri, options);
-
-
-// LOCAL DATABASE
-// var uri = process.env.MONGODB_URI;
-// // mongoose.set("useNewUrlParser", true);
-// mongoose.connect(uri, options);
-
-
-mongoose.connection.on('open', function (err, doc) {
-=======
 // Settings for online DATABASE
 var uri = process.env.MONGODB_URI;
 // mongoose.set("useNewUrlParser", true);
 mongoose.connect(uri, options);
 
-
-// LOCAL DATABASE
-// var url = 'mongodb://'+process.env.DB_HOST+':'+process.env.DB_PORT+'/'+process.env.DB_NAME;
-// mongoose.connect(url, { useNewUrlParser: true })
-//     .then(() => console.log(`Now connected to MongoDB on database: ${process.env.DB_NAME}!`))
-//     .catch(err => console.error('Something went wrong', err));
-
 mongoose.connection.on('open', function(err, doc){
->>>>>>> Development
   console.log(`connection established with ${process.env.DB_NAME}`);
 });
 // Port for running server
@@ -66,7 +43,6 @@ const port = process.env.PORT;
 const app = express();
 
 app
-    
     
     .use(urlencodedParser)
     // define static files
