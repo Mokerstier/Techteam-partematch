5b1e712791f668fb775ada8d15f17b130fedc56e
Bas de Greeuw <bas.de.greeuw@van-ons.nl>
2019-06-14 10:54:26 +0200

    Change password now available in profile (checks, changes and hashes a new password)

---
+++ controllers/change-password.js
@@ -0,0 +1,64 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const mongo = require("mongodb");
+const express = require("express");
+const session = require("express-session");
+const { userSchema } = require("../models/user");
+const bodyParser = require("body-parser");
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+module.exports = (req, res) => {
+	const user_id = req.session.passport.user;
+	console.log(`${user_id} is trying to change password`);
+	const currentPass = req.body.currentPass;
+	const newPass = req.body.newPass;
+	userSchema.findOne({ _id: user_id }, async (err, doc) => {
+		console.log(doc);
+		if (err) throw err;
+		try {
+			bcrypt.compare(currentPass, doc.password, (err, authSucces) => {
+				if (err) throw err;
+				if (authSucces) {
+					console.log(`${user_id} passwords matched`);
+					bcrypt.hash(newPass, 10, async (err, hash) => {
+						if (err) {
+							return next(err);
+						}
+						try {
+							userSchema.updateOne(
+								{ _id: user_id },
+								{ $set: { password: hash } },
+								async (err, doc) => {
+									if (err) throw err;
+									if (!doc) {
+										console.log(
+											`${user_id} pass change- Something went wrong while updating`
+										);
+										return done(null, false, {
+											message: "Something went wrong while updating"
+										});
+									}
+									console.log(`updated password ${user_id}`);
+									res.redirect("/settings");
+								}
+							);
+						} catch (err) {
+							console.log(err);
+						} finally {
+							console.log("password change complete");
+						}
+					});
+				} else {
+					return done(null, false, { message: "Wrong password" });
+				}
+			});
+		} catch (err) {
+			console.log(err);
+		} finally {
+			console.log("password tried");
+		}
+	});
+};
